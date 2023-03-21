6cd6efa29f6c6c1d2136449c1b49d8f1dd9a6e73
Supachai Vorapojpisut <vsupacha@gmail.com>
2023-03-21 14:28:18 +0700

    Update code, and try .gitignore

--- bleCollector/app.py
+++ bleCollector/app.py
@@ -3,6 +3,7 @@ import asyncio
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 
 class bleCollector:
    def __init__(self):
@@ -15,8 +16,14 @@ class bleCollector:
 
    async def co_mongo(self):
       self.mongoClient = MongoClient(os.environ['MONGODB_URI'], 27017)
+      ble_db = self.mongoClient.blecollector # Database
+      hb_collection = ble_db.heart_beat # Collection
+      count = 0
       while self.running:
          print("MongoDB is running")
+         beat = {"timestamp": datetime.now(), "count": count}
+         hb_collection.insert_one(beat)
+         count += 1
          await asyncio.sleep(1)
    
    async def co_mqtt(self):
